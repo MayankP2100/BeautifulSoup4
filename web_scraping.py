d7e657b1ec6ec3138eeab377a2b5806a67b0218d
Mayank <palmayank2100@gmail.com>
2021-10-15 23:19:16 +0530

    get price for the url item

---
+++ web_scraping.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+
+with open("index.html", "r") as f:
+    doc = BeautifulSoup(f, "html.parser")
+
+# print(doc.prettify())
+
+url = "https://www.newegg.ca/samsung-1tb-980/p/N82E16820147804?Item=N82E16820147804&cm_sp=Dailydeal_SS-_-20-147-804-_-10152021"
+
+result = requests.get(url)
+doc2 = BeautifulSoup(result.text, "html.parser")
+
+# print(doc2.prettify())
+
+prices = doc.find_all(text="$")
+parent = prices[0].parent
+strong = parent.find("strong") # checks for the strong html tag
+print(strong.string)
